a43f0e4f9d46098a5e4bd2b159d57e40adcc835b
KeyPan-GitHub <13651220389@163.com>
2020-08-11 16:04:15 +0800

    spider-1

---
+++ MaoYanTop100/spider.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   spider.py
+@Time    :   2020/06/08 18:33:46
+@Author  :   辛酉 
+@Version :   1.0
+@Desc    :   None
+'''
+
+# here put the import lib
+
+import requests
+from requests.exceptions import RequestException
+
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+def main():
+    url = 'https://maoyan.com/board/4?'
+    html = get_one_page(url)
+
+
+
+if __name__ == "__main__":
+    main()
