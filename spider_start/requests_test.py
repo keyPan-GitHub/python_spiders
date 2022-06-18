c9c1396363d8e269963b65a3e77fb15e77fc65b1
key-Pan <13651220389@163.com>
2022-06-18 19:35:11 +0800

    requests学习

--- spider_start/requests_test.py
+++ spider_start/requests_test.py
@@ -5,7 +5,7 @@ res = requests.get('https://www.seejav.bid/forum/forum.php?mod=forumdisplay&fid=
 # print(res.text)
 print(res.encoding)
 print(res.status_code)
-# print(res.cookies)
-# print(res.content)
+print(res.cookies)
+print(res.text)
 
 
