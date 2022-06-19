f907cd6e5b9d490db15618876a6f30495ef8c290
key-Pan <13651220389@163.com>
2022-06-19 17:49:24 +0800

    re模块 match方法 serch方法 sub方法 分别作用于搜索，替换。

--- spider_start/reg_test.py
+++ spider_start/reg_test.py
@@ -1,1 +1,21 @@
 import re
+# 提取字符串
+# 替换
+# 搜索
+
+info = "姓名:bobby 生日:1987年10月1日 本科：2005年9月1日"
+# print(re.findall("\d{4}", info))
+# print(re.match(".*生日.*\d{4}", info))
+# print(re.search("生日.*\d{4}", info))
+match = re.match(".*生日.*?(\d{4}).*本科.*?(\d{4})", info)
+print(match.group(1))
+print(match.group(2))
+
+sub = re.sub("\d{4}", "2019", info)
+print(sub)
+
+# name = 'my name is Bobby'
+name = """my name is 
+bobby   
+"""
+print(re.match('.*bobby', name, re.DOTALL).group())
